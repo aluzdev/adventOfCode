68144c1771c9a4ed670392e89dc6384fb97cfe61
Aluz <aluzdev@gmail.com>
2023-12-04 02:41:18 -0700

    im getting close but its 3 am come on

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,79 @@ const SmallLogos = styled.div`
   margin: 0;
 `;
 
+const P = styled.p`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const input = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`;
+
+const symbols = ["#", "$", "%", "&", "*", "+", "-", "/", "=", "@"];
+
+function lookForNumbers(schematic, i, j) {
+  let sum = 0;
+  const somethingWentTerriblyWrong =
+    i < 0 ||
+    i >= schematic.length ||
+    j < 0 ||
+    j >= schematic[i].length ||
+    !symbols.includes(schematic[i][j]);
+  if (somethingWentTerriblyWrong) return;
+  const current = schematic[i][j];
+  const diagLeft = schematic[i + 1][j - 1];
+  const top = schematic[i + 1][j];
+  const diagRight = schematic[i + 1][j + 1];
+  const left = schematic[i][j - 1];
+  const right = schematic[i][j + 1];
+  const diagDownLeft = schematic[i - 1][j - 1];
+  const down = schematic[i - 1][j];
+  const diagDownRight = schematic[i - 1][j + 1];
+
+  console.log({
+    current,
+    diagLeft,
+    top,
+    diagRight,
+    left,
+    right,
+    diagDownLeft,
+    down,
+    diagDownRight,
+  });
+}
+
 function idk(schematic) {
   const cachedSchematic = JSON.parse(sessionStorage.getItem("schematic"));
-  const newSchematic =
+  const transformedSchematic =
     cachedSchematic ||
     transformArray(schematic.split("\n").map((row) => row.split("")));
   if (!cachedSchematic) {
-    sessionStorage.setItem("schematic", JSON.stringify(newSchematic));
+    sessionStorage.setItem("schematic", JSON.stringify(transformedSchematic));
   }
 
-  const setOfSymbols = new Set();
-  for (let row of newSchematic) {
-    // console.log(row)
-    for (let char of row) {
-      // console.log(char);
-      setOfSymbols.add(char);
+  for (let i = 0; i < transformedSchematic.length; i++) {
+    for (let j = 0; j < transformedSchematic[i].length; j++) {
+      // console.log(transformedSchematic[i][j]);
+      const character = transformedSchematic[i][j];
+      if (symbols.includes(character)) {
+        lookForNumbers(transformedSchematic, i, j);
+      }
     }
-    // console.log(setOfSymbols);
   }
-  return setOfSymbols;
+  // console.log(char);
 }
+
+idk(input);
+console.log(sessionStorage.getItem("schematic").split(","));
 
 function App() {
   return (
@@ -48,8 +101,7 @@ function App() {
         </a>
       </SmallLogos>
       <h1>Advent of Code Day 2</h1>
-      {/* <p>{[...idk(schematic)].sort()}</p> */}
-      <p>{sessionStorage.getItem("schematic")}</p>
+      <P>{sessionStorage.getItem("schematic").split(",")}</P>
     </>
   );
 }
